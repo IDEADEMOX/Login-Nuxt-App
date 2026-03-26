fd5f1918a1c966a307e28f6acc7ac8e72ce05d9c
zhengjialux <15879398507@163.com>
2026-03-26 16:35:44 +0800

    修改密钥生成工具

--- utils/jwt.util.ts
+++ utils/jwt.util.ts
@@ -1,22 +1,19 @@
-import { SignJWT, jwtVerify } from "jose";
+// import { SignJWT, jwtVerify } from "jose";
+import jwt from "jsonwebtoken";
 
-const secretKey = process.env.SESSION_SECRET;
-const encodedKey = new TextEncoder().encode(secretKey);
+const secretKey = process.env.SESSION_SECRET || "default-secret-key";
+// const encodedKey = new TextEncoder().encode(secretKey);
 
 export async function encrypt(
   payload: Record<string, any>,
-  expiresIn: string = "7d",
+  expiresIn: jwt.SignOptions["expiresIn"] = "7d",
 ) {
-  return new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime(expiresIn)
-    .sign(encodedKey);
+  return jwt.sign(payload, secretKey, { expiresIn });
 }
 
 export async function decrypt(session: string | undefined = "") {
   try {
-    const { payload } = await jwtVerify(session, encodedKey, {
+    const payload = jwt.verify(session, secretKey, {
       algorithms: ["HS256"],
     });
     return payload;
